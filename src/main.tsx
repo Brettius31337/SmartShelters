3f3ab4788b7cdd6ec6d14f2160308f1711c9f34c
user.email <uber@uberpi.local>
2026-04-03 11:12:59 -0400

    fix: clean untracked + final scaffold

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,26 @@
-import React from 'react'\nimport ReactDOM from 'react-dom/client'\nimport App from './App.tsx'\n\nReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SuiClientProvider, WalletProvider, createNetworkConfig } from '@mysten/dapp-kit'
+import { getFullnodeUrl } from '@mysten/sui'
+import '@mysten/dapp-kit/dist/index.css'
+
+const queryClient = new QueryClient()
+
+const { networkConfig } = createNetworkConfig({
+ testnet: { url: getFullnodeUrl('testnet') },
+ mainnet: { url: getFullnodeUrl('mainnet') },
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+ <React.StrictMode>
+ <QueryClientProvider client={queryClient}>
+ <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+ <WalletProvider>
+ <App />
+ </WalletProvider>
+ </SuiClientProvider>
+ </QueryClientProvider>
+ </React.StrictMode>,
 )
